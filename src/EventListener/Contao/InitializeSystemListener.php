abc7df16550440424e56d53a258a6898e7678b1e
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-10-31 20:32:37 +0100

    do not allow multi column editor in form generator

---
+++ src/EventListener/Contao/InitializeSystemListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace HeimrichHannot\MultiColumnEditorBundle\EventListener\Contao;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+#[AsHook('initializeSystem')]
+class InitializeSystemListener
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private ScopeMatcher $scopeMatcher,
+    )
+    {
+    }
+
+    public function __invoke(): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request || !$this->scopeMatcher->isBackendRequest($request)) {
+            return;
+        }
+
+        // do not load the multiColumnEditor in the form generator
+        if ('form' === $request->query->get('do') && 'tl_form_field' === $request->query->get('table') && 'edit' === $request->query->get('act')) {
+            unset($GLOBALS['TL_FFL']['multiColumnEditor']);
+        }
+    }
+}
